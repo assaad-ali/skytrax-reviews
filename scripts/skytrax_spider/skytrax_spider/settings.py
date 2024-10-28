8ce11a4085b25413d9abfe69e92f9c4c49120218
Ali-Assaad <aliassaadtr@gmail.com>
2024-10-28 23:16:06 +0200

    Added User-Agent settings

--- scripts/skytrax_spider/skytrax_spider/settings.py
+++ scripts/skytrax_spider/skytrax_spider/settings.py
@@ -14,7 +14,10 @@ NEWSPIDER_MODULE = "skytrax_spider.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "skytrax_spider (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " \
+             "AppleWebKit/537.36 (KHTML, like Gecko) " \
+             "Chrome/112.0.0.0 Safari/537.36"
+
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
