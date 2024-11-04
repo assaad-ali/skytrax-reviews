f29f60a9884fd7f74d5736c844643b8cbb5f1868
Ali-Assaad <aliassaadtr@gmail.com>
2024-11-04 22:50:57 +0200

    Added text cleaning and spelling correction functions

--- scripts/british_airways_reviews_processing.py
+++ scripts/british_airways_reviews_processing.py
@@ -28,3 +28,16 @@ spell = SpellChecker()
 
 df = pd.read_csv("../data/raw/british_airways_raw_reviews.csv")
 logger.info(f"Loaded raw data with {len(df)} reviews.")
+
+def clean_text(text):
+    text = re.sub(r"(✅\s*Trip\s*Verified\s*\|\s*|Not\s*Verified\s*\|\s*)", "", text, flags=re.IGNORECASE)
+    text = re.sub(r"[^\w\s]", " ", text)
+    text = re.sub(r"\s+", " ", text)
+    return text.strip()
+
+def correct_spelling(text):
+    corrected_text = []
+    for word in text.split():
+        corrected_word = spell.correction(word)
+        corrected_text.append(corrected_word if corrected_word else word)
+    return ' '.join(corrected_text)
